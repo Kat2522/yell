6aa3d31b5e815de08ad6b87166e00b5375baf160
Kathrine Howell <Kehowell2522@gmail.com>
2022-05-01 13:55:09 -0700

    added a new string to yell

--- yell.js
+++ yell.js
@@ -5,3 +5,7 @@ function yell(str) {
 
   console.log(yell("I want to go to the store")); // => "I WANT TO GO TO THE STORE!!!"
   console.log(yell("Time to program")); // => "TIME TO PROGRAM!!!"
+
+  // adding something to see if i can push it in
+
+  console.log(yell("Woot it is working for me")); // "WOOT IT IS WORKING FOR ME"
