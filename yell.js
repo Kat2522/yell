abbaa2d701ced2c5be71c3e15d86131049858b4a
Kathrine Howell <Kehowell2522@gmail.com>
2022-05-01 13:27:01 -0700

    this should cap all letters in a string

---
+++ yell.js
@@ -0,0 +1,7 @@
+function yell(str) {
+    let msg = str.toUpperCase();
+    return msg;// your code here...
+  }
+
+  console.log(yell("I want to go to the store")); // => "I WANT TO GO TO THE STORE!!!"
+  console.log(yell("Time to program")); // => "TIME TO PROGRAM!!!"
